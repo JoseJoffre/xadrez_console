67eb5f66bf8d1e3ba90e1f0a801c51bc5fffef46
JoseJoffre <j.joffre.galvao@gmail.com>
2020-07-20 12:43:43 -0300

    Criacao da classe PosicaXadrez e seus metodo de conversao

---
+++ xadrez-console/Xadrez/PosicaoXadrez.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using tabuleiro;
+
+namespace Xadrez
+{
+    class PosicaoXadrez
+    {
+    public char coluna { get; set; }
+    public int linha { get; set; }
+
+        public PosicaoXadrez(char coluna, int linha)
+        {
+            this.coluna = coluna;
+            this.linha = linha;
+        }
+        public override string ToString()
+        {
+            return ""+ coluna + linha;
+        }
+        public Posicao toPosicao()
+        {
+            return new Posicao(8 - linha, coluna - 'a');
+        }
+    }
+}
