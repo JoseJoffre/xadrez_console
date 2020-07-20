0234d76d96958ff086d9b4b6b072bd0389b16985
JoseJoffre <j.joffre.galvao@gmail.com>
2020-07-19 22:01:38 -0300

    Começando a colocar peças no tabuleiro

---
+++ xadrez-console/Xadrez/Torre.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using tabuleiro;
+
+namespace Xadrez
+{
+    class Torre : Peca
+    {
+        public Torre(Tabuleiro tab, Cor cor) : base(tab, cor)
+        {
+
+        }
+        public override string ToString()
+        {
+            return "T";
+        }
+    }
+}
--- xadrez-console/tabuleiro/Peca.cs
+++ xadrez-console/tabuleiro/Peca.cs
@@ -9,9 +9,9 @@ namespace tabuleiro
 
         public Tabuleiro tab { get; protected set; }
 
-        public Peca(Posicao posicao, Cor cor, Tabuleiro tab)
+        public Peca(Tabuleiro tab, Cor cor)
         {
-            this.posicao = posicao;
+            this.posicao = null;
             this.cor = cor;
             this.tab = tab;
             this.qteMovimentos = 0;
