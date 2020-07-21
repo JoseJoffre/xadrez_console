340527bae7aeaee552bce907f8eadc099b39e51c
JoseJoffre <j.joffre.galvao@gmail.com>
2020-07-21 16:54:12 -0300

    Implementando resticoes de movimento.

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -8,36 +8,43 @@ namespace xadrez_console
     {
         static void Main(string[] args)
         {
-            try
-            {
-                PartidaDeXadrez partida = new PartidaDeXadrez();
 
-                while (!partida.terminada)
+            PartidaDeXadrez partida = new PartidaDeXadrez();
+
+            while (!partida.terminada)
+            {
+                try
                 {
                     Console.Clear();
                     Tela.imprimirTabuleiro(partida.tab);
                     Console.WriteLine();
-                    
+                    Console.WriteLine("Aguardando jogada: " + partida.jogadorAtual);
+
+                    Console.WriteLine();
                     Console.Write("Digite a posição de Origem: ");
                     Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
-                    
+                    partida.validarPosicaoDeOrigem(origem);
+
                     bool[,] posicoesPossiveis = partida.tab.peca(origem).movimentosPossiveis();
                     Console.Clear();
                     Tela.imprimirTabuleiro(partida.tab, posicoesPossiveis);
-                    
+
                     Console.Write("Digite a posição de Destino: ");
                     Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
+                    partida.validarPosicaoDestino(origem, destino);
 
-                    partida.executaMovimento(origem, destino);
+                    partida.realizaJogada(origem, destino);
+                } catch(TabuleiroException e)
+                {
+                    Console.WriteLine(e.Message);
+                    Console.ReadLine();
+                }  
 
-                }
-                
-                
             }
-            catch (TabuleiroException e)
-            {
-                Console.WriteLine(e.Message);
-            }
+
+
+
+
         }
     }
 }
